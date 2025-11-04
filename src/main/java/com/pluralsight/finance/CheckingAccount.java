f401db57ed3983d0b567158498c9513dd5d23a4e
leecatrina <cninalee3@gmail.com>
2025-11-04 11:50:09 -0500

    create field members, constructor, getters and setters

---
+++ src/main/java/com/pluralsight/finance/CheckingAccount.java
@@ -0,0 +1,22 @@
+package com.pluralsight.finance;
+
+public class CheckingAccount extends Account {
+    public CheckingAccount(String name, String accountNumber, double balance) {
+        super(name, accountNumber, balance);
+    }
+
+    @Override
+    public double getValue() {
+        return getBalance();
+    }
+
+    public double deposit(double amount) {
+        double newBalance = getBalance() + amount;
+        setBalance(newBalance);
+    }
+
+    public double withdraw(double amount) {
+        double newBalance = getBalance() - amount;
+        setBalance(newBalance);
+    }
+}
