6c2fb49d3c1a26c2d9d4bf2dda3b489145b5ca78
leecatrina <cninalee3@gmail.com>
2025-11-04 14:07:11 -0500

    start Portfolio

---
+++ src/main/java/com/pluralsight/finance/CreditCard.java
@@ -0,0 +1,23 @@
+package com.pluralsight.finance;
+
+public class CreditCard extends Account {
+    public CreditCard(String name, String accountNumber, double balance) {
+        super(name, accountNumber, balance);
+    }
+
+    public  double charge(double amount){
+        double newBalance = getBalance() + amount;
+        setBalance(newBalance);
+        return newBalance;
+    }
+    public double pay(double amount){
+        double newBalance = getBalance() - amount;
+        setBalance(newBalance);
+        return newBalance;
+    }
+
+    @Override
+    public double getValue() {
+        return getBalance();
+    }
+}
---
+++ src/main/java/com/pluralsight/finance/Jewelry.java
@@ -0,0 +1,16 @@
+package com.pluralsight.finance;
+
+public class Jewelry extends FixedAsset{
+    private double karat;
+
+    public Jewelry(String name, double marketValue, double karat) {
+        super(name, marketValue);
+        this.karat = karat;
+    }
+
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+}
